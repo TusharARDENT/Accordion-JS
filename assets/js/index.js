4de206e781b83df6b24d001d2083dec315dd2f5f
TusharKadamARDENT <tushar.kadam@ardentisys.com>
2024-11-26 19:58:17 +0530

    styled Accordion for display = block in index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,18 @@
 // Functionality of the page should appear here
+console.log("Hello Accordion")
+
+let accButton = document.getElementsByClassName("acc-listHeading")
+console.log(accButton);
+
+let accButton2 = document.querySelectorAll(".acc-listHeading")
+console.log(accButton2)
+
+let listDesc = document.querySelectorAll(".acc-listDesc")
+// listDesc.style.color = 'blue';
+console.log(listDesc)
+
+for(let i = 0; i < listDesc.length;i++){
+    if(listDesc[i].style.display = "block"){
+        listDesc[i].style.display = "none"
+    }
+}
