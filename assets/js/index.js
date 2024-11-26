9c58a79f0bae7631e10a0bba77bb73a7ebdff73f
TusharArdent <tushar.kadam@ardentisys.com>
2024-11-27 01:39:32 +0530

    Added basic dropdown functionality in the index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,37 @@
 // Functionality of the page should appear here
-console.log("Hello Accordion")
+console.log("Hello Accordion");
 
-let accButton = document.getElementsByClassName("acc-listHeading")
+let accButton = document.getElementsByClassName("acc-listHeading");
 console.log(accButton);
 
-let accButton2 = document.querySelectorAll(".acc-listHeading")
-console.log(accButton2)
+let accButton2 = document.querySelectorAll(".acc-listHeading");
+console.log(accButton2);
 
-let listDesc = document.querySelectorAll(".acc-listDesc")
+let listDesc = document.querySelectorAll(".acc-listDesc");
 // listDesc.style.color = 'blue';
-console.log(listDesc)
+console.log(listDesc);
 
-for(let i = 0; i < listDesc.length;i++){
-    if(listDesc[i].style.display = "block"){
-        listDesc[i].style.display = "none"
-    }
+const dropdown = document.querySelectorAll(".acc-dropdown");
+
+for (let i = 0; i < listDesc.length; i++) {
+  if ((listDesc[i].style.display = "block")) {
+    listDesc[i].style.display = "none";
+  }
 }
+
+function rotate(dropdown) {
+  dropdown.style.transition = "transform (180deg)";
+}
+accButton2.forEach((button) => {
+  button.addEventListener("click", function () {
+    const content = button.nextElementSibling;
+    if (content.style.display == "none") {
+      content.style.display = "block";
+      content.style.transition = "transform 2sec ease";
+      rotate(dropdown);
+    } else if (content.style.display == "block") {
+      content.style.display = "none";
+    }
+    content.style.transition = ", 0.3s ease";
+  });
+});
